b03021b29582f37e8c7b2c5a1691ebfbcb8b2c3b
HAMMAD <goforward8@hotmail.com>
2016-01-19 01:21:08 -0800

    ok2

--- app.js
+++ app.js
@@ -2,26 +2,27 @@
  * Created by HAMMAD on 1/10/2016.
  */
 
-var App =angular.module('mainApp',['ngMaterial']);
+var App =angular.module('mainApp',['ngMaterial','ngMdIcons']);
 App.controller('ctrl1',['$scope',function($scope){
    // $scope.nv='as';
-    $scope.myObject={ val: 'title'};
+   // $scope.myObject={ val: 'title'};
     $scope.myArray=[
-        {val :"To do app",status : false}
+        {val :"To do Item",status : false}
     ];
 
-
-
-
-
-
+ $scope.totalItems=$scope.myArray.length;
+$scope.remain;
     $scope.add = function ()
     {
-        //$scope.myObject.val=$scope.newItem;
-        $scope.myArray.push({val:$scope.newItem , status:false});
-        //{val:$scope.newItem}
+        if($scope.newItem)
+            $scope.myArray.push({val: $scope.newItem, status: false});
     };
     $scope.delete=function()
     {
+            $scope.myArray=$scope.myArray.filter(function(item){
+                remain= !item.status;
+                return remain;
+            })
+
     };
 }]);
